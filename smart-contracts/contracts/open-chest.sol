0f07877b4fbe4e663bbca6587e5f63e54ad10cfa
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-11-03 22:12:36 +0000

    Add Dapp with 95% complete styling for v1

---
+++ smart-contracts/contracts/open-chest.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+// A contract that allows you to open a chest and receive a random NFT.
+// import chest from "./chest.sol";
+// contract OpenChest {
+//     IERC721 public chest;
+//     IERC721 public item;
+
+//     constructor(address _chest, address _item) {
+//         chest = IERC721(_chest);
+//         item = IERC721(_item);
+//     }
+
+//     function open(uint256 _chestId) public {
+//         chest.transferFrom(msg.sender, address(this), _chestId);
+//         uint256[] memory items = new uint256[](3);
+//         for (uint256 i = 0; i < 3; i++) {
+//             items[i] = item.mint(msg.sender);
+//         }
+//         chest.burn(_chestId);
+//     }
+// }

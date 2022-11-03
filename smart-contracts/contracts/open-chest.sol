09696e18bb4f36e64dba803f2234c7a9809cb259
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-11-03 22:44:06 +0000

    Add note to smart-contracts directory

--- smart-contracts/contracts/open-chest.sol
+++ smart-contracts/contracts/open-chest.sol
@@ -1,5 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
+
+This section is a work in progress for a more complex future DAPP
+
+
+
 // A contract that allows you to open a chest and receive a random NFT.
 // import chest from "./chest.sol";
 // contract OpenChest {
